c08e991e584189b1729552b6eec9fad724c92df1
고현민 <junseol86@gmail.com>
2018-05-16 23:36:10 +0900

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <console></console>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Console from './components/Console.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Console
   }
 }
 </script>
@@ -22,7 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Console.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="console">
+    <table id="wrapper">
+      <tr>
+        <td id="panelCon">
+        </td>
+        <td id="mapCon">
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'console',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
