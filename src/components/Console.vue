10e77957cb0ce2b0174bb88d9311bc5d1ad74783
고현민 <junseol86@gmail.com>
2018-05-17 23:57:27 +0900

    Add google map component

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,26 +1,73 @@
 <template>
   <div id="console">
-    <table id="wrapper">
-      <tr>
-        <td id="panelCon">
-        </td>
-        <td id="mapCon">
-        </td>
-      </tr>
-    </table>
+    <div id="wrapper">
+      <table id="consoleTable"
+      :style="{
+        width: `${sizes.winW}px`,
+        height: `${sizes.winH}px`
+      }">
+        <tr>
+          <td id="topbar"
+          colspan="2"
+          :style="{
+            height: `${sizes.topbarH}px`,
+            lineHeight: `${sizes.topbarH}px`
+          }">
+            경주도로공사 지진관리 시스템
+          </td>
+        </tr>
+        <tr>
+          <td id="panelCon"
+          :style="{
+            width: `${sizes.panelW}px`
+          }">
+          </td>
+          <td id="mapCon">
+            <google-map
+            :sizes="sizes">
+            </google-map>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 
+import GoogleMap from './google_map/GoogleMap'
+
 export default {
   name: 'console',
   components: {
-    // HelloWorld
+    GoogleMap
+  },
+  data () {
+    return {
+      sizes: {
+        winW: 0,
+        winH: 0,
+        panelW: 480,
+        topbarH: 56
+      }
+    }
+  },
+  methods: {
+    setSizes () {
+      this.sizes.winW = window.innerWidth
+      this.sizes.winH = window.innerHeight
+    }
+  },
+  mounted () {
+    this.setSizes()
+    let setSizes = this.setSizes
+    window.addEventListener('resize', function () {
+      setSizes()
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  @import '../assets/scss/console.scss';
 </style>
---
+++ src/components/google_map/GoogleMap.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="google-map">
+    <div id="wrapper">
+      <div id="map"
+      :style="{
+        width: `${sizes.winW - sizes.panelW - 1}px`,
+        height: `${sizes.winH - sizes.topbarH}px`
+      }">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'google-map',
+  props: ['sizes'],
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    window.tryInitMap()
+  }
+}
+</script>
